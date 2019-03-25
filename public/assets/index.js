116794f05512e9839eef8d79611770d8e016bc65
Arnab Biswas <arnabdbst.99@gmail.com>
2019-03-25 23:06:56 +0530

    Fixing Loding Animation

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,6 +1,5 @@
 let popUp = document.getElementById("pop-up-bg");
 let exam = document.getElementById("exam");
-let modal = document.querySelector(".modal");
 function showSem(){
     popUp.style.display = "block"
 }
@@ -10,20 +9,29 @@ function hideSem(){
     document.getElementById("hidden-input").value = exam.textContent;
 }
 let tinput = document.querySelector('.tinput');
+function showLoader() {
+    $("#progressbar").css("display", "");
+}
+
+function hideLoader() {
+    setTimeout(function () {
+        $("#progressbar").css("display", "none");
+    }, 1000);
+}
 function validateMyForm()
-{        
-    modal.style.display = "block";
+{            
     let bool = true;
     $.ajax({
         type: "POST",
         url: "/",
         async: false,        
+        beforeSend: function () { showLoader(); },
+        success: function () { hideLoader(); },
         data:{ 
             username:tinput.value,
             semester:exam.textContent,
         },                
-        }).done(function(data){
-            modal.style.display = "none";      
+        }).done(function(data){     
 		    if(data == "undefined"){
                 bool = false;
                 tinput.value = '';
